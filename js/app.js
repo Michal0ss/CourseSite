481ee1d7230ec6e46d2dda0d3f50799b2d40d63a
Michal0ss <michal.bialas.pol@gmail.com>
2025-04-06 21:06:36 +0200

    created card-slider

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   loadPartners();
   loadCategoryDropdown(setCategory);
 
+
   const searchInput = document.getElementById("course-search");
   if (searchInput) {
     searchInput.addEventListener("input", () => {
@@ -31,3 +32,35 @@ function setCategory(newCategory) {
 
   loadCourses(query, currentCategory);
 }
+
+
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+
+    // Pagination bullets
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
